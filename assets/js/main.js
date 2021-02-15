b903ce2d975183febcafcbdbcc791e5674b4c95c
IrfanEzani <irfanezani@gmail.com>
2021-02-15 19:20:35 +0800

    edit scholarship specific pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,6 @@
 */
 !(function($) {
   "use strict";
-
   // Smooth scroll for the navigation menu and links with .scrollto classes
   var scrolltoOffset = $('#header').outerHeight() - 15;
   $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
