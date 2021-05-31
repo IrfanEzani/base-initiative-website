abc4c04a53a4b2c5e23c2c09f771309041cfa485
IrfanEzani <irfanezani@gmail.com>
2021-05-31 22:07:03 +0800

    use lozed & try loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
 
 !(function($) {
   "use strict";
+
+  $(document).ready(function() {
+    //Preloader
+    preloaderFadeOutTime = 500;
+    function hidePreloader() {
+    var preloader = $('.spinner-wrapper');
+    preloader.fadeOut(preloaderFadeOutTime);
+    }
+    hidePreloader();
+    });
+    
   // Smooth scroll for the navigation menu and links with .scrollto classes
   var scrolltoOffset = $('#header').outerHeight() - 15;
   $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
@@ -201,6 +212,11 @@
   $(window).on('load', function() {
     aos_init();
   });
+
+  //Init Lozad.js
   const observer = lozad();
   observer.observe();
+
+  //Init Loader
+
 })(jQuery);
