20a52e04c939b61a7ef08ab222a245c72a510c2f
IrfanEzani <irfanezani@gmail.com>
2021-06-06 22:00:32 +0800

    remove bx icon on JPA

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,7 @@
 
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-toggle i').toggleClass('ri-menu-line ri-close-line');
           $('.mobile-nav-overly').fadeOut();
         }
         return false;
@@ -54,12 +54,12 @@
       class: 'mobile-nav d-lg-none'
     });
     $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-md-none"><i class="icofont-navigation-menu"></i></button>');
+    $('body').prepend('<button type="button" class="mobile-nav-toggle d-md-none"><i class="ri-menu-line"></i></button>');
     $('body').append('<div class="mobile-nav-overly"></div>');
 
     $(document).on('click', '.mobile-nav-toggle', function(e) {
       $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      $('.mobile-nav-toggle i').toggleClass('ri-menu-line ri-close-line');
       $('.mobile-nav-overly').toggle();
     });
 
@@ -74,7 +74,7 @@
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-toggle i').toggleClass('ri-menu-line ri-close-line');
           $('.mobile-nav-overly').fadeOut();
         }
       }
